b67d1ef2835891795fa75988d9f9a369b52d2f83
Faik Turan <faik.turan@secomtr.com>
2023-01-30 16:18:00 +0000

    second commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome Erciyes Teknopark React Native Bootcamp!</Text>
       <StatusBar style="auto" />
     </View>
   );
