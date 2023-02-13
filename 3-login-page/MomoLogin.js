a04379263eff947c7525a7bba2f6cb7bfaf54680
Faik Turan <faik.turan@secomtr.com>
2023-02-13 15:33:23 +0000

    MomoLogin done

--- 3-login-page/MomoLogin.js
+++ 3-login-page/MomoLogin.js
@@ -1,78 +1,116 @@
 import React from 'react'
-import Constants from "expo-Constants";
+import Constants from "expo-constants";
 import {FontAwesome5} from "@expo/vector-icons";
-import { SafeAreaView, Text, TextInput, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Text, TextInput, TouchableOpacity, StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default function MomoLogin() {
   return (
-    <SafeAreaView>
-        <StatusBar style="light" />
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
 
-        <View style={styles.container}>
+      <View style={styles.content}>
         <View style={styles.textWrapper}>
-            <Text style={styles.hiText}>Merhaba!</Text>
-            <Text style={styles.userText}>FAİK TURAN</Text>
-            <Text style={styles.userText}>0123456789</Text>
+          <Text style={styles.hiText}>Merhaba!</Text>
+          <Text style={styles.userText}>FAİK TURAN</Text>
+          <Text style={styles.userText}>0123456789</Text>
         </View>
 
         <View style={styles.form}>
+          {/* https://docs.expo.io/guides/icons/ */}
+          <FontAwesome5 name="lock" style={styles.iconLock} />
 
-            <FontAwesome5 name="lock" styles={styles.iconLock} />
-
-            <TextInput
+          {/* https://reactnative.dev/docs/textinput */}
+          <TextInput
             style={styles.inputPassword}
             secureTextEntry={true}
             autoFocus={true}
             placeholder="Parolanı Gir"
-            placeholderTextColor="#929292" />
+            placeholderTextColor="#929292"
+          />
 
-            <TouchableOpacity style={styles.buttonLogin}>
-
-            </TouchableOpacity>
+          {/* https://reactnative.dev/docs/touchableopacity */}
+          <TouchableOpacity style={styles.buttonLogin}>
+            <Text style={styles.buttonLoginText}>GİRİŞ YAP</Text>
+          </TouchableOpacity>
         </View>
 
+        <View style={styles.action}>
+          <TouchableOpacity>
+            <Text style={styles.userText}>PAROLANIZI MI UNUTTUNUZ</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Text style={styles.userText}>ÇIKIŞ YAP</Text>
+          </TouchableOpacity>
         </View>
+      </View>
     </SafeAreaView>
-  )
+  );
 }
 
-const TEXT : TextStyle = {
-    color:"#fff",
+const TEXT: TextStyle = {
+    color: "#fff",
     textAlign: "center",
-}
-
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: "#b0006d",
-        paddingTop: Constants.statusBarHeight,
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#b0006d",
+      paddingTop: Constants.statusBarHeight,
     },
-    textWrapper:{
-        marginTop: 60,
-        marginBottom: 30,
+    content: {
+      paddingHorizontal: 30,
     },
-    hiText:{
-        ...TEXT,
-        fontSize: 16,
-        lineHeight: 30,
+    textWrapper: {
+      marginTop: 60,
+      marginBottom: 30,
+    },
+    hiText: {
+      ...TEXT,
+      fontSize: 20,
+      lineHeight: 50,
+      fontWeight: "bold",
     },
     userText: {
-        ...TEXT,
-        fontSize: 16,
-        lineHeight: 30,
+      ...TEXT,
+      fontSize: 16,
+      lineHeight: 30,
     },
-    form:{
-        marginBottom: 30,
+    form: {
+      marginBottom: 30,
     },
-    buttonLogin:{
-        height: 50,
-        borderRadius: 25,
-        backgroundColor:"#8d015a",
-        justifyContent: "center",
-        marginTop: 15,
+    iconLock: {
+      color: "#929292",
+      position: "absolute",
+      fontSize: 16,
+      top: 22,
+      left: 22,
+      zIndex: 10,
     },
-    iconLock:{
-        
-    }
-})
+    inputPassword: {
+      height: 60,
+      borderRadius: 30,
+      paddingHorizontal: 30,
+      fontSize: 20,
+      color: "#929292",
+      backgroundColor: "#fff",
+      textAlign: "center",
+      textAlignVertical: "center",
+    },
+    buttonLogin: {
+      height: 50,
+      borderRadius: 25,
+      backgroundColor: "#8d015a",
+      justifyContent: "center",
+      marginTop: 15,
+    },
+    buttonLoginText: {
+      ...TEXT,
+    },
+    action: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+  });
--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import HelloWorld1 from './1-HelloWorld/HelloWorld1';
 import HelloWorld2 from './1-HelloWorld/HelloWorld2';
 import TheLight from './2-the-light/TheLight';
+import MomoLogin from './3-login-page/MomoLogin';
 
 export default function App() {
  //return <HelloWorld1 />
  //return <HelloWorld2 />
- return <TheLight />
+ //return <TheLight />
+ return <MomoLogin />
 }
