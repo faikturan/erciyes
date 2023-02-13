5b8437f2bba8dd8620f967310632c0f58ad1ef54
Faik Turan <faik.turan@secomtr.com>
2023-02-13 16:39:25 +0000

    facebook login done

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import HelloWorld1 from './1-HelloWorld/HelloWorld1';
 import HelloWorld2 from './1-HelloWorld/HelloWorld2';
 import TheLight from './2-the-light/TheLight';
+import FacebookLogin from './3-login-page/FacebookLogin';
 import MomoLogin from './3-login-page/MomoLogin';
 
 export default function App() {
  //return <HelloWorld1 />
  //return <HelloWorld2 />
  //return <TheLight />
- return <MomoLogin />
+ //return <MomoLogin />
+ return <FacebookLogin />
 }
