5aa0fe0afb4ab44ddee750edc72548b458438062
FAİK TURAN <faik.turan@secomtr.com>
2023-02-14 17:36:55 +0000

    instagram feed contunie

---
+++ 5-instagram-feed/data.js
@@ -0,0 +1,70 @@
+const data = {
+    profile: {
+      avatar: require("../assets/instagram-feed-images/avatar.png"),
+    },
+    stories: [
+      {
+        id: 1,
+        avatar: require("../assets/instagram-feed-images/avatar1.jpg"),
+        name: "5.min.craft",
+        isSeen: false,
+      },
+      {
+        id: 2,
+        avatar: require("../assets/instagram-feed-images/avatar2.jpg"),
+        name: "teammonsterbox",
+        isSeen: false,
+      },
+      {
+        id: 3,
+        avatar: require("../assets/instagram-feed-images/avatar3.jpg"),
+        name: "thangflycomicsoffical",
+        isSeen: true,
+      },
+    ],
+    articles: [
+      {
+        id: 1,
+        avatar: require("../assets/instagram-feed-images/avatar1.jpg"),
+        name: "5.min.craft",
+        image: require("../assets/instagram-feed-images/img1.jpg"),
+        viewCount: "685,357",
+        date: "1 gün önce",
+      },
+      {
+        id: 2,
+        avatar: require("../assets/instagram-feed-images/avatar1.jpg"),
+        name: "5.min.craft",
+        image: require("../assets/instagram-feed-images/img2.jpg"),
+        viewCount: "1,356",
+        date: "3 gün önce",
+      },
+      {
+        id: 3,
+        avatar: require("../assets/instagram-feed-images/avatar3.jpg"),
+        name: "thangflycomicsoffical",
+        image: require("../assets/instagram-feed-images/img3.jpg"),
+        viewCount: "322,555,432",
+        date: "8 saat önce",
+      },
+      {
+        id: 4,
+        avatar: require("../assets/instagram-feed-images/avatar2.jpg"),
+        name: "teammonsterbox",
+        image: require("../assets/instagram-feed-images/img4.jpg"),
+        viewCount: "198",
+        date: "1 gün önce",
+      },
+      {
+        id: 5,
+        avatar: require("../assets/instagram-feed-images/avatar2.jpg"),
+        name: "teammonsterbox",
+        image: require("../assets/instagram-feed-images/img5.jpg"),
+        viewCount: "123,357",
+        date: "2 gün önce",
+      },
+    ],
+  };
+  
+  export default data;
+  
