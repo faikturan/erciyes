06dd2b31081eeb6559cbf1ff930f646a9f4823cc
FAİK TURAN <faik.turan@secomtr.com>
2023-03-16 16:16:24 +0100

    music player contunie

---
+++ 13-music-player/style.js
@@ -0,0 +1,9 @@
+export const PRIMARY_COLOR = "#F39220";
+export const PRIMARY_TEXT_COLOR = "#686868";
+export const SECONDARY_TEXT_COLOR = "#9a9a9a";
+
+
+export const ROW = {
+    flexDirection: "row",
+    justifyContent: "space-between",
+};
